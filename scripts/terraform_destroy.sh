1cf7a4a0dce9bd1f720908cba3cb4e834ffaf71b
durveshyadav3604 <durvesh.yadav3604@gmail.com>
2026-06-24 00:28:08 +0530

    Create terraform_destroy.sh

---
+++ scripts/terraform_destroy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "🔥 Starting Terraform Destroy..."
+
+cd "$(dirname "$0")/../terraform/main"
+
+terraform init
+terraform destroy -auto-approve
+
+echo "✅ Terraform destroy completed successfully."
